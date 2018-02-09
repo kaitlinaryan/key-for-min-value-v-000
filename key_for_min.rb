d5048b49ab2f50a43cb6b46d518e2af656ad78af
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-09 19:17:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,6 @@
 
 def key_for_min_value(name_hash)
 #{:blake => 500, :ashley => 2, :adam => 1}
-name_hash.collect { |key, value| value }
+name_hash.collect { |key, value| value }.first
+puts key
 end
